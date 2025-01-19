66fc80a82f58d7d39b6230e79ebbf2b7fdb754be
Diana Scharf <scharf.diana@googlemail.com>
2025-01-19 14:19:50 +0100

    Fix method name

--- src/Models/Embedding.php
+++ src/Models/Embedding.php
@@ -41,7 +41,7 @@ class Embedding extends Model
         $queryEmbedding = $index->embed($query);
 
         if ($queryEmbedding instanceof ErrorResponse) {
-            return 'Error: '.$queryEmbedding->getMessage();
+            return 'Error: ' . $queryEmbedding->getContent();
         }
 
         $vector = new Vector($queryEmbedding->embeddings);
@@ -49,7 +49,7 @@ class Embedding extends Model
         $nearestNeighbor = self::getNearestNeighbor($vector, $additionalConstraints);
 
         $context = $nearestNeighbor->content ?? 'No context given.';
-
+        dump($context);
         if ($contextPrompt === null) {
             $contextPrompt = $index->engine->getDefaultPrompt();
         }
